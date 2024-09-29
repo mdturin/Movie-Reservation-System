ef6d75516a9b5996b3e8d4de58301d4ee125be19
Md Turin <matkhan01@yahoo.com>
2024-09-29 21:26:26 +0600

    connected to db, seed job

--- Movi.SeedJob/Program.cs
+++ Movi.SeedJob/Program.cs
@@ -1,2 +1,32 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Movi.Core.Domain.Entities;
+using Movi.Core.Domain.Interfaces;
+using Movi.Infrastructure.Data;
+using Movi.Infrastructure.Repositories;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var config = builder.Build();
+var services = new ServiceCollection();
+services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+services.AddLogging(configure => configure.AddConsole());
+services.AddTransient<IBulkRepository, BulkRepository>();
+services
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+var serviceProvider = services.BuildServiceProvider();
+var userManager = serviceProvider
+    .GetRequiredService<UserManager<ApplicationUser>>();
+
+if (userManager.FindByEmailAsync("root@gmail.com") != null)
+    Console.WriteLine("found root.");
